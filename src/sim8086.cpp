201252cde2afa6904e26dbbb4a387a979f1de166
Lucas Fidalgo <lukefidalgo@outlook.com>
2023-03-07 13:42:21 -0300

    Homework 1 done

--- src/sim8086.cpp
+++ src/sim8086.cpp
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+bool d_is_set = false;
+bool w_is_set = true;
+bool reg_reg = false;
+
 struct Program {
     uint16_t* current_instruction;
     uint16_t* final_instruction;
@@ -40,6 +44,122 @@ Program read_binary_file(const char* filename) {
     return prog;
 }
 
+// Idk how to do bitwise operations so bear with me.
+void decodify_first_half(uint8_t* byte) {
+    d_is_set = false;
+    w_is_set = false;
+
+    // TODO: CREATE A TABLE WITH ALL THE INSTRUCTIONS
+    uint8_t value = *byte;
+    uint8_t opcode_mask = 0b11111100;
+    uint8_t dw_mask = 0b00000011;
+
+    // Is it a mov instruction?
+    if ((value & opcode_mask) == 0b10001000) {
+        printf("mov ");
+    } else {
+        printf("ERROR: instruction not recognized!\n");
+    }
+
+    // Are either d or w set?
+    if ((value & dw_mask) == dw_mask) {
+        d_is_set = true;
+        w_is_set = true;
+    }
+    else if ((value & 0b00000010) == 0b00000010) {
+        d_is_set = true;
+    }
+    else if ((value & 0b00000001) == 0b00000001) {
+        w_is_set = true;
+    }
+}
+
+void reg(uint8_t value) {
+    if (w_is_set) {
+        switch (value) {
+            case 0b00000000:
+                printf("ax");
+                break;
+            case 0b00000001:
+                printf("cx");
+                break;
+            case 0b00000010:
+                printf("dx");
+                break;
+            case 0b00000011:
+                printf("bx");
+                break;
+            case 0b00000100:
+                printf("sp");
+                break;
+            case 0b00000101:
+                printf("bp");
+                break;
+            case 0b00000110:
+                printf("si");
+                break;
+            case 0b00000111:
+                printf("di");
+                break;
+        }
+    } else {
+        switch (value) {
+            case 0b00000000:
+                printf("al");
+                break;
+            case 0b00000001:
+                printf("cl");
+                break;
+            case 0b00000010:
+                printf("dl");
+                break;
+            case 0b00000011:
+                printf("bl");
+                break;
+            case 0b00000100:
+                printf("ah");
+                break;
+            case 0b00000101:
+                printf("ch");
+                break;
+            case 0b00000110:
+                printf("dh");
+                break;
+            case 0b00000111:
+                printf("bh");
+                break;
+        }
+    }
+}
+
+void r_m(uint8_t value) {
+    if (reg_reg) {
+        reg(value);
+    } else {
+        printf("NON REG/REG OPERATIONS NOT SUPPORTED!!\n");
+    }
+}
+
+void decodify_second_half(uint8_t* byte) {
+    uint8_t value = *byte;
+
+    uint8_t mod_mask = 0b11000000;
+    if ((value & mod_mask) == mod_mask) {
+        reg_reg = true;
+    }
+    uint8_t reg_mask = 0b00111000;
+    uint8_t r_m_mask = 0b00000111;
+
+    // Will this work?!?!?!?!
+    uint8_t masked_reg_val = value & reg_mask;
+    masked_reg_val = masked_reg_val >> 3;
+
+    r_m(value & r_m_mask);
+    printf(", ");
+    reg(masked_reg_val);
+    printf("\n");
+}
+
 int main(int argc, char** argv) {
     if (argc < 2) {
         printf("USAGE: sim8086 <binary path>\n");
@@ -48,11 +168,15 @@ int main(int argc, char** argv) {
 
     Program prog = read_binary_file(argv[1]);
     
-    int i = 1;
+    puts("bits 16\n");
     while (prog.current_instruction != prog.final_instruction) {
-        printf("%d: %d\n", i, *prog.current_instruction);
+        uint8_t* byte = (uint8_t*)prog.current_instruction;
+
+        decodify_first_half(byte);
+        byte++;
+        decodify_second_half(byte);
+
         prog.current_instruction++;
-        i++;
     }
 
     // No idea how to properly free this memory :P
